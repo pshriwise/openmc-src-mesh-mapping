2d9d1837b69552135e2f2b1703d5257f9288f7f0
Patrick Shriwise <pshriwise@gmail.com>
2023-10-11 10:55:34 -0500

    Acessing mesh object

--- src_to_mesh.cpp
+++ src_to_mesh.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "openmc/capi.h"
+#include "openmc/mesh.h"
 
 int main(int argc, char** argv) {
 
@@ -9,7 +10,12 @@ int main(int argc, char** argv) {
 
   openmc_init(argc, argv, nullptr);
 
-  
+  const auto& mesh = openmc::model::meshes[openmc::model::mesh_map[1]];
+
+  std::cout << "Mesh ID: " << mesh->id() << std::endl;
+  std::cout << "Mesh bins: " << mesh->n_bins() << std::endl;
+
+  openmc_finalize();
 
   return 0;
 
