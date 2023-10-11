ed6b310f5917ca8131cbb4dc5a992d99d6271f86
Patrick Shriwise <pshriwise@gmail.com>
2023-10-11 11:41:23 -0500

    A little polish

--- src_to_mesh.cpp
+++ src_to_mesh.cpp
@@ -12,19 +12,11 @@ int main(int argc, char** argv) {
 
   std::cout << "Hello world" << std::endl;
 
-  openmc_init(argc, argv, nullptr);
+  openmc_init(0, nullptr, nullptr);
 
-  int num_sites = 10;
-
-  // sample source sites
-  std::vector<openmc::SourceSite> sites(num_sites);
-
-  uint64_t seed = 1;
-
-  for (int i = 0; i < num_sites; i++) {
-    sites[i] = openmc::sample_external_source(&seed);
-    std::cout << "Source site xyz: " << sites[i].r.x << ", " << sites[i].r.y << ", " << sites[i].r.z << std::endl;
-  }
+  int total_sites = 100000;
+  int max_sites_incr = 100;
+  int sites_sampled {};
 
   // accessing mesh
   const auto& mesh = openmc::model::meshes[openmc::model::mesh_map[1]];
@@ -32,17 +24,31 @@ int main(int argc, char** argv) {
   std::cout << "Mesh ID: " << mesh->id() << std::endl;
   std::cout << "Mesh bins: " << mesh->n_bins() << std::endl;
 
-
   std::vector<size_t> bin_counts(mesh->n_bins(), 0);
 
-  for (const auto& s : sites) {
-    int bin = mesh->get_bin(s.r);
-    bin_counts[bin] += 1;
-    std::cout << "Bin location: " << bin << std::endl;
+  while (sites_sampled < total_sites) {
+
+    int n_sites = std::min(total_sites - sites_sampled, max_sites_incr);
+
+    // sample source sites
+    std::vector<openmc::SourceSite> sites(n_sites);
+
+    uint64_t seed = 1;
+
+    for (auto& s : sites) {
+      s = openmc::sample_external_source(&seed);
+    }
+
+    sites_sampled += n_sites;
+
+    for (const auto& s : sites) {
+      int bin = mesh->get_bin(s.r);
+      bin_counts[bin] += 1;
+    }
   }
 
   std::vector<double> rel_strengths(bin_counts.begin(), bin_counts.end());
-  for (auto& rs : rel_strengths) { rs /= num_sites; }
+  for (auto& rs : rel_strengths) { rs /= sites_sampled; }
 
   // Determine total source strength
   double total_strength = 0.0;
