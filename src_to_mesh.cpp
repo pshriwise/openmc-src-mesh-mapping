e636a1ab1c16b135ac2e2cbefdcfec6aafd8b756
Patrick Shriwise <pshriwise@gmail.com>
2023-10-11 14:47:28 -0500

    Parallelizing and cleaning

--- src_to_mesh.cpp
+++ src_to_mesh.cpp
@@ -1,4 +1,6 @@
 
+#include <omp.h>
+
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -6,16 +8,18 @@
 
 #include "openmc/capi.h"
 #include "openmc/mesh.h"
+#include "openmc/settings.h"
 #include "openmc/source.h"
+#include "openmc/timer.h"
 
 int main(int argc, char** argv) {
 
-  std::cout << "Hello world" << std::endl;
+  openmc::settings::verbosity = 0;
 
   openmc_init(0, nullptr, nullptr);
 
   int total_sites = 1e6;
-  int max_sites_incr = 100;
+  int max_sites_incr = 10000;
   int sites_sampled {};
 
   // accessing mesh
@@ -26,7 +30,14 @@ int main(int argc, char** argv) {
 
   std::vector<size_t> bin_counts(mesh->n_bins(), 0);
 
-  uint64_t seed = 1;
+  std::vector<uint64_t> seeds(omp_get_max_threads());
+  for (int i = 0; i < seeds.size(); i++) seeds[i] = i;
+
+  std::cout << "Sampling source sites and mapping to the mesh...\n";
+
+  openmc::Timer timer;
+
+  timer.start();
 
   while (sites_sampled < total_sites) {
 
@@ -34,18 +45,27 @@ int main(int argc, char** argv) {
 
     // sample source sites
     std::vector<openmc::SourceSite> sites(n_sites);
-
-    for (auto& s : sites) {
-      s = openmc::sample_external_source(&seed);
+#pragma omp parallel
+{
+    uint64_t* seed = &seeds[omp_get_thread_num()];
+    #pragma omp for
+    for (int i = 0; i < sites.size(); i++) {
+      sites[i] = openmc::sample_external_source(seed);
     }
-
+}
     sites_sampled += n_sites;
 
-    for (const auto& s : sites) {
-      int bin = mesh->get_bin(s.r);
-      bin_counts[bin] += 1;
+    #pragma omp parallel for shared(bin_counts)
+    for (int i = 0; i < sites.size(); i++) {
+      bin_counts[mesh->get_bin(sites[i].r)] += 1;
     }
   }
+
+  timer.stop();
+
+  double mapping_time = timer.elapsed();
+  timer.reset();
+
 
   std::vector<double> rel_strengths(bin_counts.begin(), bin_counts.end());
   for (auto& rs : rel_strengths) { rs /= sites_sampled; }
@@ -61,11 +81,13 @@ int main(int argc, char** argv) {
   openmc::UnstructuredMesh* umesh_ptr = dynamic_cast<openmc::UnstructuredMesh*>(mesh.get());
   if (!umesh_ptr) { std::cerr << "Non-unstructured mesh used" << std::endl; return 1; }
 
+  std::cout << "Writing output file..." << std::endl;
+  timer.start();
+
   // generate a text output as CSV
   // Each row contains: OpenMC bin #, centroid (xyz), volume, rel. src strength
   std::ofstream output("mesh_src_strengths.csv");
   output << "bin, cx, cy, cz, volume, rel. src" << std::endl;
-
   for (int i = 0; i < mesh->n_bins(); i++) {
     std::stringstream line {};
     line << i << ", ";
@@ -77,8 +99,18 @@ int main(int argc, char** argv) {
     output << line.str();
 
   }
-
   output.close();
+
+  timer.stop();
+  double output_time = timer.elapsed();
+
+  // report time
+  std::cout << "--------------" << std::endl;
+  std::cout << "Time Summary: " << std::endl;
+  std::cout << "--------------" << std::endl;
+
+  std::cout << "Source mapping (" << total_sites << " sites): " << mapping_time << "s" << std::endl;
+  std::cout << "File output: " << output_time << " s" << std::endl;
 
   openmc_finalize();
 
