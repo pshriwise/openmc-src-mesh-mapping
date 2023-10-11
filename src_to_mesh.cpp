781aa664f0903b218e481e70985551277b7a1190
Patrick Shriwise <pshriwise@gmail.com>
2023-10-11 10:49:37 -0500

    Working. Initializing openmc

--- src_to_mesh.cpp
+++ src_to_mesh.cpp
@@ -1,9 +1,15 @@
 
 #include <iostream>
 
-int main() {
+#include "openmc/capi.h"
+
+int main(int argc, char** argv) {
 
   std::cout << "Hello world" << std::endl;
+
+  openmc_init(argc, argv, nullptr);
+
+  
 
   return 0;
 
